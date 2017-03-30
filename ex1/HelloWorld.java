e265a30dd212dabccbe91afc849175d1c67c98f3
Sergey Ivanov <ivsaemail@gmail.com>
2017-03-30 12:38:10 +0300

    first commit

---
+++ ex1/HelloWorld.java
@@ -0,0 +1,10 @@
+package ex1;
+
+/**
+ * Created by Dendi on 3/30/2017.
+ */
+public class HelloWorld {
+    static {
+        System.out.println("hello, World");
+    }
+}
---
+++ ex1/Main.java
@@ -0,0 +1,12 @@
+package ex1;
+
+
+/**
+ * Created by Dendi on 3/30/2006.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        new HelloWorld(); // sout "hello, World"
+    }
+}
