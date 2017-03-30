22f8101510994d6b139e4a8adab9ed1d053909d7
Sergey Ivanov <ivsaemail@gmail.com>
2017-03-30 12:42:06 +0300

    one com del/another one change

--- ex1/Main.java
+++ ex1/Main.java
@@ -1,12 +1,10 @@
 package ex1;
 
 
-/**
- * Created by Dendi on 3/30/2006.
- */
+
 
 public class Main {
     public static void main(String[] args) {
-        new HelloWorld(); // sout "hello, World"
+        new HelloWorld(); // print "hello, World"
     }
 }
